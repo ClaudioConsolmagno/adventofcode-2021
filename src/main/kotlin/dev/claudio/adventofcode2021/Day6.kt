150a44eabfca078a45ac10d894d207a69aedf003
claudio <claudio.consolmagno@rungway.com>
2021-12-06 11:53:51 +0000

    Day 6

---
+++ src/main/kotlin/dev/claudio/adventofcode2021/Day6.kt
@@ -0,0 +1,28 @@
+package dev.claudio.adventofcode2021
+
+fun main() {
+    Day6().main()
+}
+
+private class Day6 {
+    fun main() {
+        val inputList: List<Int> = Support.readFileAsListString("day6-input.txt")
+            .flatMap { it.split(",") }
+            .map { it.toInt() }
+
+        var outputList = inputList
+        (0 until 80).forEach{
+            outputList = tick(outputList)
+        }
+        println(outputList.count())
+    }
+
+    private fun tick(inputList: List<Int>): List<Int> {
+        val spawns = inputList.count {
+            it == 0
+        }
+        return inputList.map { it - 1 }
+            .map { if (it < 0) { 6 } else { it } }
+            .plus((0 until spawns).map { 8 })
+    }
+}
