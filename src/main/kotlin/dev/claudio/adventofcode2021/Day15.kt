88c746d177615b77d552ba263d7621df6853053f
claudio <claudio.consolmagno@rungway.com>
2021-12-15 20:00:16 +0000

    Day 15

---
+++ src/main/kotlin/dev/claudio/adventofcode2021/Day15.kt
@@ -0,0 +1,44 @@
+package dev.claudio.adventofcode2021
+
+import dev.claudio.adventofcode2021.Support.Companion.surroundingPoints4
+import org.jgrapht.Graph
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath
+import org.jgrapht.graph.DefaultWeightedEdge
+import org.jgrapht.graph.builder.GraphTypeBuilder
+import java.awt.Point
+
+fun main() {
+    Day15().main()
+}
+
+private class Day15 {
+
+    // https://jgrapht.org/guide/UserOverview#graph-algorithms
+    val graph: Graph<Point, DefaultWeightedEdge> = GraphTypeBuilder
+        .directed<Point, DefaultWeightedEdge>()
+        .weighted(true)
+        .edgeClass(DefaultWeightedEdge::class.java)
+        .buildGraph()
+
+    fun main() {
+        val input = Support.readFileAsListString("day15-input.txt")
+        val ySize = input.size
+        val xSize = input[0].length
+        val maxPoint = Point(xSize-1, ySize-1)
+        (0 until ySize).forEach { y ->
+            (0 until xSize).forEach { x ->
+                graph.addVertex(Point(x, y))
+            }
+        }
+        input.forEachIndexed{ y, str ->
+            str.toList().forEachIndexed { x, yStr ->
+                val currentPoint = Point(x, y)
+                currentPoint.surroundingPoints4(maxPoint).forEach {
+                    graph.addEdge(currentPoint, it).apply { graph.setEdgeWeight(this, yStr.digitToInt().toDouble()) }
+                }
+            }
+        }
+        val dijkstraAlg = DijkstraShortestPath(graph).getPaths(maxPoint)
+        println(dijkstraAlg.getWeight(Point(0,0)))
+    }
+}
--- src/main/kotlin/dev/claudio/adventofcode2021/Support.kt
+++ src/main/kotlin/dev/claudio/adventofcode2021/Support.kt
@@ -4,16 +4,16 @@ import java.awt.Point
 
 class Support {
     companion object {
-        fun readFileAsListInt(fileName: String) : List<Int> = readFileAsListString(fileName).map { Integer.valueOf(it) }
+        fun readFileAsListInt(fileName: String): List<Int> = readFileAsListString(fileName).map { Integer.valueOf(it) }
 
-        fun readFileAsListString(fileName: String) : List<String>
-                = object {}.javaClass.classLoader.getResourceAsStream(fileName)!!
-            .bufferedReader()
-            .readLines()
+        fun readFileAsListString(fileName: String): List<String> =
+            object {}.javaClass.classLoader.getResourceAsStream(fileName)!!
+                .bufferedReader()
+                .readLines()
 
         fun stringTranspose(list: List<String>): List<String> {
             val tranposedList = mutableListOf<CharArray>()
-            (1..list[0].length).forEach{
+            (1..list[0].length).forEach {
                 tranposedList.add(" ".repeat(list.size).toCharArray())
             }
             list.forEachIndexed { index1, originalEntry ->
@@ -28,9 +28,9 @@ class Support {
             val xSize: Int = this.maxOf { it.x }
             val ySize: Int = this.maxOf { it.y }
             val pointsSet = this.toSet()
-            (0 .. ySize).forEach { y ->
-                (0 .. xSize).forEach { x ->
-                    if (pointsSet.contains(Point(x,y))) {
+            (0..ySize).forEach { y ->
+                (0..xSize).forEach { x ->
+                    if (pointsSet.contains(Point(x, y))) {
                         print("# ")
                     } else {
                         print(". ")
@@ -38,7 +38,24 @@ class Support {
                 }
                 println()
             }
+        }
 
+        fun Point.surroundingPoints8(maxPoint: Point): Set<Point> {
+            return (-1..1).flatMap { x ->
+                    (-1..1).mapNotNull { y ->
+                        if (!(x == 0 && y == 0)) {
+                            Point(this.x + x, this.y + y)
+                        } else {
+                            null
+                        }
+                    }
+                }
+                .filter { it.y > -1 && it.x > -1 && it.x <= maxPoint.x && it.y <= maxPoint.y }
+                .toSet()
+        }
+
+        fun Point.surroundingPoints4(maxPoint: Point): Set<Point> {
+            return this.surroundingPoints8(maxPoint).filter { it.x == this.x || it.y == this.y }.toSet()
         }
     }
 }
